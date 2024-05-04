30397950404c6aaf76df2d95aafaa00b554e7c69
Hermione Dadheech <hermionedadheech@gmail.com>
2024-05-04 14:27:36 +0530

    feat:setting up redux store and components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { Provider } from 'react-redux';
 import './App.css';
-
+import Header from './components/Header';
+import Joblisting from './components/Joblisting';
+import { appStore } from './utils/appStore';
 function App() {
-  return <></>;
+  return (
+  <Provider store ={appStore}>
+  <Header/>
+  <Joblisting />
+  </Provider>
+)
 }
 
 export default App;
---
+++ src/utils/jobSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const jobSlice = createSlice(
+    {
+        name:"",
+        initialState:[],
+        reducers:{
+            
+        }
+    }
+)
+export default jobSlice.reducer
